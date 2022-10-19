a3eafce311dcc8fbdbb6e8a3092ecff92d1e56bc
Matheus Cendrão <matheuscendrao@gmail.com>
2022-10-19 20:21:26 -0300

    Add authorization parameters for requests. (#3)
    Some endpoints in Github API requests authentication, so it is adding
    the possibility of sending an access token to each requests.

--- lib/github_ex/client.ex
+++ lib/github_ex/client.ex
@@ -3,24 +3,57 @@ defmodule GithubEx.Client do
   Github Tesla client.
 
   It handles the HTTP calls to Github API. Can be used to make a request that is
-  not implemented on the library, for example:
+  not implemented on the library.
 
+  It always add the following default headers:
 
-  GithubEx.Client.get("/licenses")
+    * `{"User-Agent", "Githubex"}`
+    * `{"Accept", "application/vnd.github+json"}`
   """
-  use Tesla
 
-  plug(Tesla.Middleware.BaseUrl, base_url())
-  plug(Tesla.Middleware.Headers, default_headers())
-  plug(Tesla.Middleware.JSON)
+  @default_headers [{"User-Agent", "Githubex"}, {"Accept", "application/vnd.github+json"}]
 
-  defp base_url do
-    github_config = Application.get_env(:github_ex, :github)
+  @doc """
+  Makes a GET request to the given URL considering the configuration in config.exs 
+  for the base URL.
 
-    github_config.base_url
+  It is possible to pass the access token as option for authenticated requests.
+
+  ## Options
+
+    * `access_token` - will add the Authorization header to the request with Bearer <token> as the value.
+
+  ## Examples
+
+      iex> GithubEx.Client.get("/licenses")
+      iex> {:ok, %Tesla.Env{status: 200, body: "Licences response body"}}
+  """
+  @spec get(String.t(), keyword()) :: Tesla.Env.result()
+  def get(url, opts) do
+    Tesla.get(client(opts), url)
   end
 
-  defp default_headers do
-    [{"User-Agent", "Githubex"}]
+  defp client(opts) do
+    config = Application.get_env(:github_ex, :github)
+
+    headers = maybe_add_authorization_header(@default_headers, opts)
+
+    middleware = [
+      {Tesla.Middleware.BaseUrl, config.base_url},
+      {Tesla.Middleware.Headers, headers},
+      Tesla.Middleware.JSON
+    ]
+
+    Tesla.client(middleware)
+  end
+
+  defp maybe_add_authorization_header(headers, opts) do
+    case Keyword.get(opts, :access_token) do
+      nil ->
+        headers
+
+      access_token ->
+        headers ++ [{"Authorization", "Bearer #{access_token}"}]
+    end
   end
 end
